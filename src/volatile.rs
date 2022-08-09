4f388cf740eae92f7eac46e2025346bf15726bb8
nayyara-airlangga <nayyara-airlangga@gmail.com>
2022-08-10 00:12:44 +0700

    Update volatile wrapper docs

--- src/volatile.rs
+++ src/volatile.rs
@@ -3,29 +3,17 @@ use core::ptr;
 /// A wrapper type around a volatile variable, which allows for volatile reads and writes
 /// to the contained value. The stored type needs to be `Copy`, as volatile reads and writes
 /// take and return copies of the value.
-///
-/// The size of this struct is the same as the size of the contained type.
 #[derive(Debug, Default)]
 #[repr(transparent)]
 pub struct Volatile<T: Copy>(T);
 
 impl<T: Copy> Volatile<T> {
     /// Construct a new volatile instance wrapping the given value.
-    ///
-    /// # Panics
-    ///
-    /// This method never panics.
     pub fn new(value: T) -> Volatile<T> {
         Volatile(value)
     }
 
-    /// Performs a volatile write, setting the contained value to the given value `value`. Volatile
-    /// writes are guaranteed to not be optimized away by the compiler, but by themselves do not
-    /// have atomic ordering guarantees.
-    ///
-    /// # Panics
-    ///
-    /// This method never panics
+    /// Performs a volatile write, setting the contained value to the given value `value`.
     pub fn write(&mut self, src: T) {
         unsafe {
             // UNSAFE: Safe, we know that the internal value is valid/exists
@@ -34,13 +22,7 @@ impl<T: Copy> Volatile<T> {
     }
 
     /// Performs a volatile read of the contained value, returning a copy
-    /// of the read value. Volatile reads are guaranteed not to be optimized
-    /// away by the compiler, but by themselves do not have atomic ordering
-    /// guarantees.
-    ///
-    /// # Panics
-    ///
-    /// This method never panics
+    /// of the read value.
     pub fn read(&self) -> T {
         unsafe {
             // UNSAFE: Safe, we know that the internal value is valid/exists
@@ -51,10 +33,6 @@ impl<T: Copy> Volatile<T> {
     /// Performs a volatile read of the contained value, passes a mutable reference to it to the
     /// function `f`, and then performs a volatile write of the (potentially updated) value back to
     /// the contained value.
-    ///
-    /// # Panics
-    ///
-    /// This method never panics.
     pub fn update<F>(&mut self, f: F)
     where
         F: FnOnce(&mut T),

52e27e3cd51e752d97e29f420a555d257d6df10b
nayyara-airlangga <nayyara-airlangga@gmail.com>
2022-08-09 17:15:52 +0700

    Add initial welcome message for the kernel

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,19 @@
 
 use core::panic::PanicInfo;
 
+static HELLO: &[u8] = b"Welcome to BasedOS v0.1.0";
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
 

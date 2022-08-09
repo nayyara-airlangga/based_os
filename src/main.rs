688e815b61d9453387edbbef93a235a4efcb27f2
nayyara-airlangga <nayyara-airlangga@gmail.com>
2022-08-10 01:37:48 +0700

    Implement vga buffer abstraction and print macros

--- src/main.rs
+++ src/main.rs
@@ -6,23 +6,16 @@ mod volatile;
 
 use core::panic::PanicInfo;
 
-static HELLO: &[u8] = b"Welcome to BasedOS v0.1.0";
-
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    println!("Welcome to BasedOS v0.1.0");
 
     loop {}
 }
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    println!("{_info}");
+
     loop {}
 }
